bb1ecb29a9ac049cffa6ef9cfecef87acb503ab4
Michael-Anderson92 <Michael_Anderson92@hotmail.com>
2025-04-29 20:40:44 -0700

    added Analytics component to layout export

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
-import { Analytics } from '@vercel/analytics/react'
+import { Analytics } from "@vercel/analytics/react"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}<Analytics /></body>
     </html>
   );
 }
