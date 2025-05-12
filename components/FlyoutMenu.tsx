d3b4362a1d5a4677840ad078fa5224ec2e867a05
Michael-Anderson92 <Michael_Anderson92@hotmail.com>
2025-05-12 12:24:48 -0700

    placed the flyout menu as fixed to the left hand side, lower left quadrant. May need to adjust positioning style later as site changes

--- components/FlyoutMenu.tsx
+++ components/FlyoutMenu.tsx
@@ -19,45 +19,39 @@ export default function FlyoutMenu() {
   const menuVariants = {
     closed: {
       x: '-100%',
-      opacity: 0,
       transition: {
         type: 'spring',
-        stiffness: 40,
-        damping: 25,
-        duration: 1,
-        ease: 'easeInOut'
+        stiffness: 50,
+        damping: 20,
+        duration: 0.8
       }
     },
     open: {
       x: 0,
-      opacity: 1, 
       transition: {
         type: 'spring',
-        stiffness: 40,
-        damping: 25,
-        duration: 1,
-        ease: 'easeInOut',
-        delayChildren: 0.3,
-        staggerChildren: 0.1
+        stiffness: 50,
+        damping: 20,
+        duration: 0.8,
+        delayChildren: 0.5,
+        staggerChildren: 0.2
       }
     }
   };
 
   const itemVariants = {
     closed: { 
-      x: -30,
+      x: -20, 
       opacity: 0,
       transition: {
-        duration: 0.6,
-        ease: 'easeInOut'
+        duration: 0.4
       }
     },
     open: { 
-      x: 0,
+      x: 0, 
       opacity: 1,
       transition: {
-        duration: 0.6,
-        ease: 'easeInOut'
+        duration: 0.4
       }
     }
   };
@@ -66,26 +60,26 @@ export default function FlyoutMenu() {
     <AnimatePresence>
       {isOpen && (
         <motion.div
-          initial="closed"
-          animate="open"
-          exit="closed"
-          variants={menuVariants}
-          className="sticky left-0 top-0 h-screen w-64 /90 z-50 flex flex-col justify-center"
-        >
-          <div className="flex flex-col gap-8 px-8">
-            {menuItems.map((item) => (
-              <motion.a
-                key={item.title}
-                href={item.href}
-                variants={itemVariants}
-                className="text-6xl font-semibold text-white hover:text-blue-400 transition-colors"
-                onClick={() => setIsOpen(false)}
-              >
-                {item.title}
-              </motion.a>
-            ))}
-          </div>
-        </motion.div>
+        initial="closed"
+        animate="open"
+        exit="closed"
+        variants={menuVariants}
+        className="fixed bottom-[8%] left-[3%] w-64 z-50 flex flex-col justify-center"
+      >
+        <div className="flex flex-col gap-8 px-8">
+          {menuItems.map((item) => (
+            <motion.a
+              key={item.title}
+              href={item.href}
+              variants={itemVariants}
+              className="text-6xl font-semibold text-white hover:text-blue-400 transition-colors whitespace-nowrap"
+              onClick={() => setIsOpen(false)}
+            >
+              {item.title}
+            </motion.a>
+          ))}
+        </div>
+      </motion.div>
       )}
     </AnimatePresence>
   );
